1134ef7afd86c45cc3a14b1067c14a7db85d9c58
Blake Han <han.sangyeup@gmail.com>
2021-12-15 09:25:49 -0500

    Remove Console.log

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -24,7 +24,6 @@ const Info = styled.div`
 const Category = ({ name, info }) => {
   if (!name) name = "Unknown";
   if (!info) info = "Unknown";
-  console.log(info);
   return (
     <Container>
       <Name>{name}</Name>

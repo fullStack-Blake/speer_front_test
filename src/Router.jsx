39a55359889ac1fa208fdb7f761a5491bc2b1282
Blake Han <han.sangyeup@gmail.com>
2021-12-15 04:01:07 -0500

    Add Header

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { HashRouter, Route, Redirect, Switch } from "react-router-dom";
-// import Header from "./Header.jsx";
+import Header from "./Header.jsx";
 import Feed from "./Feed.jsx";
 import Archive from "./Archive.jsx";
 import Detail from "./Detail.jsx";
@@ -9,7 +9,7 @@ const Router = () => {
   return (
     <div>
       <HashRouter>
-        {/* <Header /> */}
+        <Header />
         <Switch>
           <Route path="/" exact component={Feed} />
           <Route path="/archive" exact component={Archive} />
